c60a5d69dbacaa274d299228ffe4670b61b6c820
laurcam <lucas.arcamone@gmail.com>
2026-04-29 10:13:39 -0400

    Add the get_mri_maps attribute to retrieve all files from the qmri folder, and the get_qmri_maps_grouped attribute to store each filename organized by subject and session in a dictionary

--- registrabids/core/bids_index.py
+++ registrabids/core/bids_index.py
@@ -1,18 +1,41 @@
 from bids import BIDSLayout
+from collections import defaultdict
 
 class BIDSIndex:
     def __init__(self, root):
-        self.layout = BIDSLayout(root, derivatives=True)
+        self.layout = BIDSLayout(root,
+                                 derivatives=[f"{root}/derivatives/qmri"])
 
     def get_subjects(self):
         return self.layout.get_subjects()
 
     def get_derivatives(self):
         return self.layout.get(scope="derivatives")
-    
-    def get_qmri_maps(self):
-        return self.layout.get(
-        scope="derivatives/qmri",
-        suffix=None,
-        extension=[".nii", ".nii.gz"]
-        )
+
+    def get_qmri_maps(self, subjects=None, sessions=None, suffixes=None):
+        query = {
+            "scope": "qmri",
+            "extension": [".nii", ".nii.gz"]
+        }
+
+        if subjects is not None:
+            query["subject"] = subjects
+
+        if sessions is not None:
+            query["session"] = sessions
+
+        if suffixes is not None:
+            query["suffix"] = suffixes
+
+        return self.layout.get(**query)    
+
+    def get_qmri_maps_grouped(self, subjects=None, sessions=None):
+        files = self.get_qmri_maps(subjects, sessions)
+
+        grouped = defaultdict(list)
+
+        for f in files:
+            key = (f.entities.get("subject"), f.entities.get("session"))
+            grouped[key].append(f)
+
+        return grouped
